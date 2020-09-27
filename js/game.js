2d36e8264e80bfa7bf0af8aefe3088ecd08353b8
nickmackenzie <nick-m@live.ca>
2020-09-27 15:57:11 -0400

    basic ui

---
+++ js/game.js
@@ -0,0 +1,12 @@
+/*----- constants -----*/
+let playerBoard = document.querySelector("div#game-board");
+let space = document.querySelectorAll("div.square");
+/*----- app's state (variables) -----*/
+let playerTurn = -1;
+/*----- cached element references -----*/
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function playerClick(e) {
+  console.log("her");
+}

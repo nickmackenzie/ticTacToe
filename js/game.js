576b1d5b9be815bcb3300cc1dde55b93beb2d45b
nickmackenzie <nick-m@live.ca>
2020-09-29 19:58:33 -0400

    logic is done for win/lose

--- js/game.js
+++ js/game.js
@@ -1,34 +1,75 @@
 /*----- constants -----*/
 let playerBoard = document.querySelector("#game-board");
-
+let btn = document.querySelector("button")
+let square = document.querySelectorAll(".square");
+let winMsg = document.getElementById("winMsg")
 /*----- app's state (variables) -----*/
 let playerTurn = -1;
-let board = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
+let boardArray = [
+  [, ,],
+  [, ,],
+  [, ,],
 ];
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 playerBoard.addEventListener("click", playerClick);
+btn.addEventListener("click", resetBoard)
 /*----- functions -----*/
 function playerClick(e) {
-  board.forEach(function (boardSquare, idx) {
-    boardSquare.forEach(function (sqaure, id) {
-      console.log(sqaure);
-    });
-  });
+  let squareId = e.target.id;
+  let rowId = e.target.dataset.row;
+  console.log(rowId, squareId);
+  if (e.target.textContent == "O" || e.target.textContent == "X") {
+    console.log("already taken");
+    return;
+  }
   if (playerTurn === -1 && e.target.textContent === "") {
-    console.log("player one turn");
     playerTurn = playerTurn + 1;
-    console.log("player has clicked square ", e.target.id);
     e.target.textContent = "X";
-    console.log(playerTurn);
+    boardArray[rowId].splice(squareId, 1, 1);
+    console.log(boardArray);
+    myFunction();
   } else if (playerTurn === 0 && e.target.textContent === "") {
-    console.log("player two turn");
     playerTurn = playerTurn - 1;
     e.target.textContent = "O";
-    console.log("player has clicked square ", e.target.id);
-    console.log(playerTurn);
+    boardArray[rowId].splice(squareId, 1, 2);
+    myFunction()
   }
+}
+
+
+
+function myFunction() {
+  for (let i = 0; i < 3; i++) {
+    let horizonalWin = boardArray[i][0] + boardArray[i][1] + boardArray[i][2];
+    let verticalWin = boardArray[0][i] + boardArray[1][i] + boardArray[2][i];
+    let diagonalWinOne = boardArray[0][0] + boardArray[1][1] + boardArray[2][2]
+    let diagonalWinTwo = boardArray[0][2] + boardArray[1][1] + boardArray[2][0]
+    if (horizonalWin === 6 || diagonalWinOne === 6 || diagonalWinTwo === 6 || verticalWin === 6) {
+      winMessage = "O has won"
+      winMsg.append(winMessage)
+      return;
+    } else if
+      (horizonalWin === 3 || diagonalWinOne === 3 || diagonalWinTwo === 3 || verticalWin === 3) {
+      winMessage = "x has won"
+      winMsg.append(winMessage)
+      return
+    }
+  }
+}
+
+function resetBoard(e) {
+  e.preventDefault()
+
+  for (i = 0; i < square.length; i++) {
+    square[i].textContent = ""
+  }
+  playerTurn = -1;
+  boardArray = [
+    [, ,],
+    [, ,],
+    [, ,],
+  ];
+  winMsg.textContent = ""
 }
